748f5b56ac96ecb4a1f93e56700e08a044054b9a
Andre Nepomuceno <andrenepomuceno>
2021-05-02 00:01:47 -0300

    Minor updates

--- nodemcu-32s/src/main.cpp
+++ nodemcu-32s/src/main.cpp
@@ -6,19 +6,27 @@
 #include <time.h>
 #include "secret.h"
 
-const char indexHtml[] PROGMEM = R"EOF(<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"><title>ESP Garden</title></head><body><div class="container"><h1>ESP Garden</h1><h2>Status</h2><table class="table table-striped"><tbody id="tbody-status"></tbody></table><h2>Inputs</h2><table class="table table-striped"><thead><tr><th scope="col">#</th><th scope="col">Port</th><th scope="col">Value</th></tr></thead><tbody id="tbody-inputs"></tbody></table><h2>Outputs</h2><table class="table table-striped"><thead><tr><th scope="col">#</th><th scope="col">Port</th><th scope="col">Value</th></tr></thead><tbody id="tbody-outputs"></tbody></table></div><script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script><script>function fillTable(id, data, index=true){ var tbody=$(id); tbody.empty(); for (var i=0; i < data.length; i++){ var tr=$('<tr/>'); if (index) tr.append($('<th/>').html(i).attr("scope", "row")); for (var key in data[i]){ tr.append($('<td/>').html(key)); tr.append($('<td/>').html(data[i][key]));} tbody.append(tr);}} function refresh(){ setTimeout(refresh, 1 * 1000); var info={ "Status": [ { "Date": Date(Date.now())}, { "Time": "time"}, { "Uptime": "uptime"} ], "Inputs": [ { "a0": (Math.random() * 4096).toFixed()}, { "a1": (Math.random() * 4096).toFixed()}, { "a2": (Math.random() * 4096).toFixed()} ], "Outputs": [ { "gpio0": "0"}, { "gpio1": "1"}, { "gpio2": "2"} ]}; $.getJSON("/data.json", function (info){ fillTable("#tbody-status", info["Status"], false); fillTable("#tbody-inputs", info["Inputs"]); fillTable("#tbody-outputs", info["Outputs"]);});} $(function onReady(){ refresh();}); </script></body><footer></footer></html>
+#define syncClock() configTime(0, -3 * 60 * 60, "pool.ntp.org")
+
+const char indexHtml[] PROGMEM = R"EOF(<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"><title>ESP Garden</title></head><body><div class="container"><h1>ESP Garden</h1><h2>Status</h2><table class="table table-striped"><tbody id="tbody-status"></tbody></table><h2>Inputs</h2><table class="table table-striped"><thead><tr><th scope="col">#</th><th scope="col">Port</th><th scope="col">Value</th></tr></thead><tbody id="tbody-inputs"></tbody></table><h2>Outputs</h2><table class="table table-striped"><thead><tr><th scope="col">#</th><th scope="col">Port</th><th scope="col">Value</th></tr></thead><tbody id="tbody-outputs"></tbody></table><script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script><script>function fillTable(id, data, index=true){ var tbody=$(id); tbody.empty(); for (var i=0; i < data.length; i++){ var tr=$('<tr/>'); if (index) tr.append($('<th/>').html(i).attr("scope", "row")); for (var key in data[i]){ tr.append($('<td/>').html(key)); tr.append($('<td/>').html(data[i][key]));} tbody.append(tr);}} function refresh(){ setTimeout(refresh, 1 * 1000); $.ajax({ dataType: "json", url: "/data.json", timeout: 500, success: function (info){ fillTable("#tbody-status", info["Status"], false); fillTable("#tbody-inputs", info["Inputs"]); fillTable("#tbody-outputs", info["Outputs"]);}});} $(function onReady(){ refresh();}); </script></body><footer></footer></html>
 )EOF";
 
-const int ADC_READ_SIZE = 4;
-const int GPIO_READ_SIZE = 4;
+const int ADC_READ_SIZE = 3;
+const int A0_INDEX = 0;
+const int A3_INDEX = 1;
+const int A6_INDEX = 2;
+
+const int GPIO_READ_SIZE = 2;
+const int GPIO0_INDEX = 0;
+const int GPIO23_INDEX = 1;
 
 WebServer server(80);
 WiFiClient client;
 
 time_t bootTime = 0;
+int tsErrors = 0;
 uint16_t adcRead[ADC_READ_SIZE];
 uint8_t gpioRead[GPIO_READ_SIZE];
-int tsErrors = 0;
 
 void handleRoot()
 {
@@ -53,14 +61,14 @@ void handleDataJson()
   json += "],";
 
   json += "\"Inputs\":[";
-  json += "{\"A0\":\"" + String(adcRead[0]) + "\"},";
-  json += "{\"A3\":\"" + String(adcRead[1]) + "\"},";
-  json += "{\"A6\":\"" + String(adcRead[2]) + "\"},";
-  json += "{\"GPIO0\":\"" + String(gpioRead[0]) + "\"}";
+  json += "{\"A0\":\"" + String(adcRead[A0_INDEX]) + "\"},";
+  json += "{\"A3\":\"" + String(adcRead[A3_INDEX]) + "\"},";
+  json += "{\"A6\":\"" + String(adcRead[A6_INDEX]) + "\"},";
+  json += "{\"GPIO0\":\"" + String(gpioRead[GPIO0_INDEX]) + "\"}";
   json += "],";
 
   json += "\"Outputs\":[";
-  json += "{\"GPIO23\":\"" + String(gpioRead[1]) + "\"}";
+  json += "{\"GPIO23\":\"" + String(gpioRead[GPIO23_INDEX]) + "\"}";
   json += "]";
 
   json += "}";
@@ -100,21 +108,21 @@ void sensorsTask(void *)
 
   for (;;)
   {
-    adcRead[0] = analogRead(A0);
-    adcRead[1] = analogRead(A3);
-    adcRead[2] = analogRead(A6);
-
-    gpioRead[0] = digitalRead(0);
-    gpioRead[1] = digitalRead(23);
-
     vTaskDelay(pdMS_TO_TICKS(1000));
+
+    adcRead[A0_INDEX] = analogRead(A0);
+    adcRead[A3_INDEX] = analogRead(A3);
+    adcRead[A6_INDEX] = analogRead(A6);
+
+    gpioRead[GPIO0_INDEX] = digitalRead(0);
+    gpioRead[GPIO23_INDEX] = digitalRead(23);
   }
 }
 
 void tsTask(void *)
 {
   ThingSpeak.begin(client);
-  ThingSpeak.setField(3, bootTime);
+  ThingSpeak.setField(8, bootTime);
 
   for (;;)
   {
@@ -122,8 +130,8 @@ void tsTask(void *)
 
     digitalWrite(LED_BUILTIN, 1);
 
-    ThingSpeak.setField(1, adcRead[0]);
-    ThingSpeak.setField(2, adcRead[1]);
+    ThingSpeak.setField(1, adcRead[A0_INDEX]);
+    ThingSpeak.setField(2, adcRead[A3_INDEX]);
     int status = ThingSpeak.writeFields(channelNumber, apiKey);
     if (status != 200)
     {
@@ -132,6 +140,24 @@ void tsTask(void *)
     }
 
     digitalWrite(LED_BUILTIN, 0);
+  }
+}
+
+void clockUpdateTask(void *)
+{
+  int minutes = 0;
+
+  for (;;)
+  {
+    vTaskDelay(pdMS_TO_TICKS(60 * 1000));
+
+    ++minutes;
+    if (minutes >= 24 * 60)
+    {
+      minutes = 0;
+      Serial.println("Updating clock...");
+      syncClock();
+    }
   }
 }
 
@@ -174,13 +200,14 @@ void setup(void)
   server.begin();
   Serial.println("HTTP server started");
 
-  configTime(0, -3 * 60 * 60, "pool.ntp.org");
+  syncClock();
   delay(1000);
   bootTime = time(NULL);
 
   xTaskCreate(sensorsTask, "sensorsTask", 1024, NULL, 2, NULL);
   xTaskCreate(serverTask, "serverTask", 4 * 1024, NULL, 2, NULL);
   xTaskCreate(tsTask, "tsTask", 4 * 1024, NULL, 2, NULL);
+  xTaskCreate(clockUpdateTask, "clockUpdateTask", 4 * 1024, NULL, 1, NULL);
 }
 
 void loop(void)
